8d9d3367f7c09807ab444131306d66490ad70c0b
muazmuaz786 <koreanmuaz@gmail.com>
2025-02-16 14:19:55 +0500

    Update settings.py

--- chat_project/settings.py
+++ chat_project/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-gy^o7zgex=wzmzqt@j4i+x7hur=ps(@5*-m3*m3e-qamer$5st'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
